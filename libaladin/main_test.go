7b8283550af5d9eb4d639aacd643efd8f1677787
YeoCheon Yun <ycyun@ycyun.xyz>
2022-01-26 11:47:39 +0900

    테스트 코드 수정

--- libaladin/main_test.go
+++ libaladin/main_test.go
@@ -5,9 +5,8 @@ import (
 	"testing"
 )
 
-
 func TestHello(t *testing.T) {
-	s:=Hello("name")
+	s := Hello("name")
 	fmt.Println(s)
 	if s != "Hello, name" {
 		t.Error("Wrong result")
@@ -26,14 +25,12 @@ func TestHello(t *testing.T) {
 //}
 
 func TestSearchBook(t *testing.T) {
-	s:= SearchBook("소드 아트")
+	s := SearchBook("소드 아트")
+	fmt.Println(t.Name())
 	for _, book := range s {
 		fmt.Printf("%8v원, %v\n", book.PriceSales, book.Title)
 	}
 	fmt.Printf("%v Books Searched", len(s))
-	//a, err:=json.MarshalIndent(s, "", " ")
-	//if err!=nil{
-	//	_ = fmt.Errorf("%v", err)
-	//}
-	//fmt.Println(string(a))
+	fmt.Println("")
+	return
 }
